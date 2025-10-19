64ee3634470902c68070f19ba2f5424184eb8da7
xiaojian <jianxiao97@outlook.com>
2025-10-19 22:36:23 +0800

    add maps, slices example

---
+++ 10.maps/maps.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"maps"
+)
+
+func main() {
+	m := make(map[string]int)
+
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	v2 := m["k2"]
+	fmt.Println("v2:", v2)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	clear(m)
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n and n2 are equal")
+	}
+}
